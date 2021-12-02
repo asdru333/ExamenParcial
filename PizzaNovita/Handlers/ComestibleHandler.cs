69fe86ed9337a043581593d358f5d1e0d081dfac
asdru333 <asdrubal.villegas.molina@hotmail.com>
2021-12-01 21:09:31 -0600

    BaseDatosHandlerAgregado

---
+++ PizzaNovita/Handlers/ComestibleHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System;
+using PizzaNovita.Models;
+
+namespace PizzaNovita.Handlers
+{
+    public class ComestibleHandler
+    {
+    }
+}
